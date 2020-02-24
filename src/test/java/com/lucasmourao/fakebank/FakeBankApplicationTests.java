47468e1e3ae790f1ff7f31aa70d6ff79d3b8f906
LucasMouraoFerreira <lucas.mo.ferr@gmail.com>
2020-02-23 21:20:14 -0300

    Project Created

---
+++ src/test/java/com/lucasmourao/fakebank/FakeBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasmourao.fakebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
