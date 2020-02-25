ba2f4699cefd4d99bc0f2bc75a059db7280414f1
LucasMouraoFerreira <lucas.mo.ferr@gmail.com>
2020-02-25 16:08:49 -0300

    Account - Put

--- src/main/java/com/lucasmourao/fakebank/resources/AccountResource.java
+++ src/main/java/com/lucasmourao/fakebank/resources/AccountResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,4 +49,11 @@ public class AccountResource {
 		service.deleteById(id);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@PutMapping(value="/{id}")
+	public ResponseEntity<Account> updateAccount(@PathVariable long id, @RequestBody Account acc){
+		acc = service.updateAccount(id, acc);
+		return ResponseEntity.ok().body(acc);
+	}
+	
 }
--- src/main/java/com/lucasmourao/fakebank/services/AccountService.java
+++ src/main/java/com/lucasmourao/fakebank/services/AccountService.java
@@ -11,24 +11,50 @@ import com.lucasmourao.fakebank.repositories.AccountRepository;
 
 @Service
 public class AccountService {
-	
+
 	@Autowired
 	private AccountRepository repository;
-	
-	public List<Account> findAll(){
+
+	public List<Account> findAll() {
 		return repository.findAll();
 	}
-	
-	public Account findById(long id){
+
+	public Account findById(long id) {
 		Optional<Account> acc = repository.findById(id);
 		return acc.get();
 	}
-	
+
 	public Account insertAccount(Account acc) {
 		return repository.save(acc);
 	}
-	
+
 	public void deleteById(long id) {
 		repository.deleteById(id);
+	}
+
+	public Account updateAccount(long id, Account acc) {
+		Optional<Account> accAux = repository.findById(id);
+		return updateData(accAux.get(), acc);
+	}
+
+	private Account updateData(Account accAux, Account acc) {
+
+		if (acc.getOwnerAddress() != null) {
+			accAux.setOwnerAddress(acc.getOwnerAddress());
+		}
+		if (acc.getOwnerCpf() != null) {
+			accAux.setOwnerCpf(acc.getOwnerCpf());
+		}
+		if (acc.getOwnerName() != null) {
+			accAux.setOwnerName(acc.getOwnerName());
+		}
+		if (acc.getPassword() != null) {
+			accAux.setPassword(acc.getPassword());
+		}
+		if (acc.getAccountActive() != null) {
+			accAux.setAccountActive(acc.getAccountActive());
+		}
+
+		return repository.save(accAux);
 	}
 }
