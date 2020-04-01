231c1f4ad43ff66f92dc0a949245a703d5a76778
LucasMouraoFerreira <lucas.mo.ferr@gmail.com>
2020-04-01 10:57:16 -0300

    JWT and Spring Security dependencies

--- src/main/java/com/lucasmourao/fakebank/repositories/UserRepository.java
+++ src/main/java/com/lucasmourao/fakebank/repositories/UserRepository.java
@@ -10,7 +10,7 @@ import com.lucasmourao.fakebank.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
 
-	@Query("SELECT u FROM User u where (u.userName = :userName) or (u.fullName = :fullName) or (u.cpf = :cpf)")
+	@Query("SELECT u FROM User u where (u.userName = :userName) or (UPPER(u.fullName) like UPPER(concat('%', :fullName,'%'))) or (u.cpf = :cpf)")
 	Page<User> fullSearch(@Param("userName") String userName, @Param("fullName") String fullName,
 			@Param("cpf") String cpf, Pageable pageable);
 		
